49db372673e0d88d691c0866cff5bf7bb6fe4a7a
ttp_admin <datateamttpservices@gmail.com>
2026-03-18 16:41:57 +0400

    Fix vite build config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173,
     proxy: {
